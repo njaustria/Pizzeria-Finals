c3725abfdc15faf2c5241c8f040553039802a74c
njaustria <neianaustria07@gmail.com>
2026-01-14 16:35:32 +0800

    Jade - added placeholder AlternativeEmailSender class

---
+++ includes/alternative-email.php
@@ -0,0 +1,16 @@
+<?php
+class AlternativeEmailSender
+{
+	/**
+	 * Send an email (placeholder implementation)
+	 * @param string $to
+	 * @param string $subject
+	 * @param string $body
+	 * @param bool $isHTML
+	 * @return bool
+	 */
+	public function send($to, $subject, $body, $isHTML = true)
+	{
+		return false;
+	}
+}
